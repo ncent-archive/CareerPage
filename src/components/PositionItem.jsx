2d646842948674694ecfabce7e2d202aab3df774
HAQish <mytrueemailaddress@gmail.com>
2019-02-21 14:21:31 -0800

    position list and items

---
+++ src/components/PositionItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class PositionItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    //bindings
+
+  }
+  //functions
+
+  render() {
+    return (
+      <div>
+        position item
+      </div>
+    )
+  }
+}
+
+export default PositionItem;
