1837965f336fc55a67d075989977ee966072614f
Adam Foosaner <nucat176@gmail.com>
2019-03-15 13:17:37 -0700

    a few UI fixes, and adds create challenge button to top right of admin page

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,110 +1,117 @@
 import React from "react";
 import store from "./../store/initializeStore.js";
-import { fetchAllChallenges } from "./../actions/challengeActions.js";
+import {fetchAllChallenges} from "./../actions/challengeActions.js";
 import Challenge from "./Challenge.jsx";
 import ChallengeDetails from "./ChallengeDetails.jsx";
+import logo from "../img/ncent_NOsubline_500px_white.png";
 
 class AdminPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      spinner: true,
-      challengeDetail: false,
-      challengeDetailIdx: null,
-      redirect: ""
-    };
-    //bindings
-    this.switchToDetails = this.switchToDetails.bind(this);
-    this.switchToCollection = this.switchToCollection.bind(this);
-    this.processParams = this.processParams.bind(this);
+    constructor(props) {
+        super(props);
+        this.state = {
+            spinner: true,
+            challengeDetail: false,
+            challengeDetailIdx: null,
+            redirect: ""
+        };
+        //bindings
+        this.switchToDetails = this.switchToDetails.bind(this);
+        this.switchToCollection = this.switchToCollection.bind(this);
+        this.processParams = this.processParams.bind(this);
 
-  }
-  //functions
+    }
 
-  async componentWillMount() {
-    this.processParams();
-    store.dispatch(fetchAllChallenges());
-  }
+    //functions
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("compDidUpdate in AdminPage", "\nprevProps", prevProps, "\nthis.props", this.props);
-    if (!prevProps.challengesReceived && this.props.challengesReceived) {
-      console.log("\nAdminPage.jsx, turning off spinner");
-      this.setState({ spinner: false }, () => {
-        //select last challenge if redirect param
-        if (this.state.redirect) {
-          let idx = this.container.children.length - 1;
-          this.switchToDetails(idx);
+    async componentWillMount() {
+        this.processParams();
+        store.dispatch(fetchAllChallenges());
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("compDidUpdate in AdminPage", "\nprevProps", prevProps, "\nthis.props", this.props);
+        if (!prevProps.challengesReceived && this.props.challengesReceived) {
+            console.log("\nAdminPage.jsx, turning off spinner");
+            this.setState({spinner: false}, () => {
+                //select last challenge if redirect param
+                if (this.state.redirect) {
+                    let idx = this.container.children.length - 1;
+                    this.switchToDetails(idx);
+                }
+            });
         }
-      });
     }
-  }
 
-  processParams() {
-    let str = window.location.href;
-    let paramsStr = str.split("?")[1];
-    let obj = {};
-    if (paramsStr) {
-      let paramsArr = paramsStr.split("&");
-      for (let pair of paramsArr) {
-        let splitPair = pair.split("=");
-        obj[splitPair[0]] = splitPair[1];
-      }
+    processParams() {
+        let str = window.location.href;
+        let paramsStr = str.split("?")[1];
+        let obj = {};
+        if (paramsStr) {
+            let paramsArr = paramsStr.split("&");
+            for (let pair of paramsArr) {
+                let splitPair = pair.split("=");
+                obj[splitPair[0]] = splitPair[1];
+            }
+        }
+        this.setState(obj);
     }
-    this.setState(obj);
-  }
 
-  switchToDetails(idx) {
-    this.setState({
-      challengeDetail: true,
-      challengeDetailIdx: idx
-    });
-  }
-
-  switchToCollection() {
-    this.setState({
-      challengeDetail: false,
-      challengeDetailIdx: null
-    });
-    if (window.location.href.includes("?")) {
-      window.history.pushState({}, "", `${window.location.origin + window.location.pathname}`);
+    switchToDetails(idx) {
+        this.setState({
+            challengeDetail: true,
+            challengeDetailIdx: idx
+        });
     }
-  }
 
-  render() {
-    if (this.state.spinner) {
-      return (
-        <div className="spinnerContainer">
-          <div className="spinner">
-          </div>
-        </div>
-      )
-    } else {
-      if (this.state.challengeDetail) {
-        return (
-          <ChallengeDetails
-            data={this.props.challengesData[this.state.challengeDetailIdx]}
-            switchToCollection={this.switchToCollection}
-          />
-        )
-      } else {
-        return (
-          <div className="huntCollectionContainer" ref={el => this.container = el}>
-            {this.props.challengesData.map((challenge, i) => {
-              return (
-                <Challenge
-                  data={challenge}
-                  key={i}
-                  switchToDetails={this.switchToDetails}
-                  idx={i}
-                />
-              )
-            })}
-          </div>
-        )
-      }
+    switchToCollection() {
+        this.setState({
+            challengeDetail: false,
+            challengeDetailIdx: null
+        });
+        if (window.location.href.includes("?")) {
+            window.history.pushState({}, "", `${window.location.origin + window.location.pathname}`);
+        }
     }
-  }
+
+    render() {
+        if (this.state.spinner) {
+            return (
+                <div className="spinnerContainer">
+                    <div className="spinner">
+                    </div>
+                </div>
+            )
+        } else {
+            if (this.state.challengeDetail) {
+                return (
+                    <ChallengeDetails
+                        data={this.props.challengesData[this.state.challengeDetailIdx]}
+                        switchToCollection={this.switchToCollection}
+                    />
+                )
+            } else {
+                return (
+                    <div className="huntCollectionContainer" ref={el => this.container = el}>
+                        <div className="headerContainer">
+                            <img className="landingLogoImg" src={logo} onError={this.imgError}/>
+                            <a href={`${window.location.origin}/createChallenge`}><span className="createChallengeBtn">+</span></a>
+                        </div>
+                        <div className="detailsSpacer"></div>
+                        {this.props.challengesData.map((challenge, i) => {
+                            return (
+                                <Challenge
+                                    data={challenge}
+                                    key={i}
+                                    switchToDetails={this.switchToDetails}
+                                    idx={i}
+                                />
+                            )
+                        })}
+                    </div>
+                )
+            }
+        }
+    }
 }
 
 export default AdminPage;
